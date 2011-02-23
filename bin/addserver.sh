c1666b8f7c728023847921de0b4f924db8726eae
Enrico Cavalli <cavalli@cilea.it>
2011-02-23 15:29:13 +0100

    baco sulla directory di installazione

--- bin/addserver.sh
+++ bin/addserver.sh
@@ -1,22 +1,20 @@
 #!/bin/bash -e
-INSTALLDIR=$( (cd -P $(dirname $0) && pwd) )
+INSTALLDIR=$( (cd -P $(dirname $0) && pwd) | sed -e 's!/bin!!' )
+
 if [ ! "${1}" ]; then
 
-echo "------"
-echo "Usage:"
-echo "$0 server_to_backup.cilea.it"
-echo ""
+	echo "Usage: $0 server_to_backup.cilea.it"
 
 else 
-. $INSTALLDIR/etc/rsbackup.conf
-if [ ! -d "$BACKUP_DIR/$1/.work" ]; then
+	. $INSTALLDIR/etc/rsbackup.conf
+	if [ ! -d "$BACKUP_DIR/$1/.work" ]; then
 
-	mkdir $BACKUP_DIR/$1
-	btrfs subvolume create $BACKUP_DIR/$1/.work
+		mkdir $BACKUP_DIR/$1
+		btrfs subvolume create $BACKUP_DIR/$1/.work
 
-	# do not backup with legato
-	echo "+skip: *" > $BACKUP_DIR/$1/.nsr
+		# do not backup with legato
+		echo "+skip: *" > $BACKUP_DIR/$1/.nsr
 
-fi
+	fi
 
 fi
