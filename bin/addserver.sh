b0d2b73a5259752c07f3f2fa0e0af44f02e58741
Riccardo 'Rik' Valzorio <valzorio@cilea.it>
2011-02-23 14:59:02 +0100

    Aggiunto controllo su parametro mancante (USAGE)

--- bin/addserver.sh
+++ bin/addserver.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
 INSTALLDIR=$( (cd -P $(dirname $0) && pwd) )
+if [ -n $1 ]; then
 
- 
- 
+echo "------"
+echo "Usage:"
+echo "./addserver.sh server_to_backup.cilea.it"
+echo ""
+
+else 
 . $INSTALLDIR/etc/rsbackup.conf
-
 if [ ! -d "$BACKUP_DIR/$1/.work" ]; then
 
 	mkdir $BACKUP_DIR/$1
@@ -11,5 +16,7 @@ if [ ! -d "$BACKUP_DIR/$1/.work" ]; then
 
 	# do not backup with legato
 	echo "+skip: *" > $BACKUP_DIR/$1/.nsr
+
+fi
 
 fi
