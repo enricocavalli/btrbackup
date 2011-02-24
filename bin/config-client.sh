18f2db8c3754e7b03dc6606b5252eee940003f11
Enrico Cavalli <cavalli@cilea.it>
2011-02-24 16:50:06 +0100

    prima versione script di configurazione client sarebbe meglio che la chiave dsa venisse presa al volo

---
+++ bin/config-client.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+groupadd rsbackup
+useradd  -s /bin/bash -d /home/rsbackup -m -g rsbackup rsbackup
+
+cat >>/etc/sudoers <<EOF
+
+Cmnd_Alias RSYNC_RSBACKUP = /usr/bin/rsync
+rsbackup ALL=(ALL) NOPASSWD: RSYNC_RSBACKUP
+
+EOF
+
+echo "sshd: 131.175.9.105" >> /etc/hosts.allow
+
+echo "Controlla iptables e routing (devi accettare ssh da 131.175.9.105)"
+
+mkdir /home/rsbackup/.ssh
+
+cat >> /home/rsbackup/.ssh/authorized_keys <<EOF
+ssh-dss AAAAB3NzaC1kc3MAAACBAMJ80GrRP6kIfJTpv0NZwFvFvqWwXYMyFu2VsHVPgGUkbNZnjhUugeIdESlBof+mYqogVicHZnX4ogCkTTlrvrmV0lzfJ0QNWicj/FDa4hP68ZaRj1dv+iy2sMUpIiKuVzYkGJVj0VZjwBQS5BVHbc7/JDuMgQHsX21x6FWm/FipAAAAFQD6+U48YN5JUo96KWAaKaZOS94qmwAAAIEAiaF6Yoj7RRVk56LHU85mfOZ5wDAlenn9qAugpvDYonJUrMYXMYG7ijw1DvsGhlIslO5YWOt2EBikoSs8+YQSEZ+v7aHB8s151kBsmerCSFv22xcpPsWnirTPynnVtd+ZCBo1Jl0Ee4iEyt6NGxrCHX2fxcY71p2746MVB3jzZLMAAACAF8kXYpBnkJK2fXKL7bym+e3h40/m29VoOpPoJzA6X8lJTbluHiqqCkE7PbNylmhS9x3/ozKAYNvfnCEhIoLP9fj/U6+fs4PwI6QbaS2m4fO3AIATCOari7CGiYpScMVrAHjcEXMFe/6SB2goNc/vpedXRAp/9WwokfrnyskSvTg= rsbackup-key
+EOF
+
+chown -R rsbackup: /home/rsbackup/.ssh/
