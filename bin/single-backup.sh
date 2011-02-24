93f93bcd9481ebf661b8b56d0688c5250dae3fc4
root <root@esx-simca.cilea.it>
2011-02-24 12:06:51 +0100

    spostate in alto le funzioni per il calcolo data (devono essere definte prima)

--- bin/single-backup.sh
+++ bin/single-backup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # ARG1: host da backuppare (e.g. esx-aiace.ciela.it)
 #
@@ -33,6 +33,25 @@ trap "rm -f $LOCK > /dev/null 2>&1" exit
 
 set +e
 
+date2stamp () {
+    date --utc --date "$1" +%s
+}
+
+dateDiff (){
+    case $1 in
+        -s)   sec=1;      shift;;
+        -m)   sec=60;     shift;;
+        -h)   sec=3600;   shift;;
+        -d)   sec=86400;  shift;;
+        -w)   sec=604800;  shift;;
+        *)    sec=86400;;
+    esac
+    dte1=$(date2stamp $1)
+    dte2=$(date2stamp $2)
+    diffSec=$((dte2-dte1))
+    if ((diffSec < 0)); then abs=-1; else abs=1; fi
+    echo $((diffSec/sec*abs))
+}
 
 	# importo varibili "globali"
 	. $INSTALLDIR/etc/rsbackup.conf
@@ -93,7 +112,6 @@ if [  0 = $return -o 24 = $return ]; then
 		if [ $ore -lt 24 ]; then
                         hourly[$ore]=$((${hourly[$ore]}+1))
                         if [ ${hourly[$ore]} -gt 1 -a $ore -ge 1 ]; then
-                        echo " remove"    >> $REPORTLOG 2>&1
                         btrfs subvolume delete $BACKUP_DIR/$RSYNC_HOST/$line >> $LOGFILE 2>&1
                         else
                         kept=$(($kept + 1))
@@ -115,7 +133,6 @@ if [  0 = $return -o 24 = $return ]; then
                         btrfs subvolume delete $BACKUP_DIR/$RSYNC_HOST/$line >> $LOGFILE 2>&1
                         else
                         kept=$(($kept + 1))
-                        echo " keep"    >> $REPORTLOG 2>&1
                         fi
 
                 fi
@@ -139,24 +156,4 @@ fi
 
 savelog $INSTALLDIR/logs/$RSYNC_HOST/rsync.log
 
-date2stamp () {
-    date --utc --date "$1" +%s
-}
-
-
-dateDiff (){
-    case $1 in
-        -s)   sec=1;      shift;;
-        -m)   sec=60;     shift;;
-        -h)   sec=3600;   shift;;
-        -d)   sec=86400;  shift;;
-        -w)   sec=604800;  shift;;
-        *)    sec=86400;;
-    esac
-    dte1=$(date2stamp $1)
-    dte2=$(date2stamp $2)
-    diffSec=$((dte2-dte1))
-    if ((diffSec < 0)); then abs=-1; else abs=1; fi
-    echo $((diffSec/sec*abs))
-}
 
