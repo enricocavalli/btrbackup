6a50fffa9a647ceeda1e958375b53479da77d185
root <root@esx-simca.cilea.it>
2011-02-25 16:28:42 +0100

    modified:   scheduler.sh
    Test scheduler - OK

--- bin/scheduler.sh
+++ bin/scheduler.sh
@@ -3,26 +3,34 @@
 # Script per l'esecuzione dei backup
 
 ##### conf
-MAX_BACKUPS=3
+MAX_BACKUPS=2
 
 #####
+INSTALLDIR=$( (cd -P $(dirname $0) && pwd) | sed -e 's!/bin!!' )
+if [ ! "${2}" ]; then
+
+	echo "Usage: $0 config1 config2 config3 ..."
+else
+
 
 set -m
-INSTALLDIR=$( (cd -P $(dirname $0) && pwd) | sed -e 's!/bin!!' )
-HOSTS=$( cd $INSTALLDIR/etc/hosts/; ls | sed -r 's!.conf!!' )
-hosts_array=( $HOSTS )
+HOSTS=$@
 
 for i in $HOSTS; do
 	
+	if ! [ -f $INSTALLDIR/etc/hosts/$i.conf ]; then
+       		echo "Configuration file $INSTALLDIR/etc/hosts/$i.conf not found"
+        	exit 1
+	fi
+
 	echo ""
 	echo '********'
 	echo "Running $i"
 	echo '********'
 	echo ""
-	time=$RANDOM
-	((time=10+$time%30))
-	#echo $time
-	sleep $time &
+	
+	# command
+	$INSTALLDIR/bin/single-backup.sh $i &
 
 	while [ 1 ]; do
 		# jobs in corso ...
@@ -43,4 +51,4 @@ for i in $HOSTS; do
 	#	((index=$index+1))
 	#fi
 done
-
+fi
