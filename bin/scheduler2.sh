687cb9f0e6a3bad00022f4ab184809f533df93f2
root <root@esx-simca.cilea.it>
2011-02-28 16:48:02 +0100

    new file:   bin/scheduler.sh
    Prime prove con scheduler. Utilizzati i file .conf di ogni host ed il comando "at" .
    
    new file:   bin/scheduler2.sh
    
    Evoluzione scheduler - processo "demonizzato" con file di configurazione in etc/scheduler.conf
    Prima bozza di:
    	- gestione backup concorrenti con lockfile generato in base alla stringa dei backup in corso
    	- polling di 10 s

---
+++ bin/scheduler2.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# demone scheduler
+INSTALLDIR=$( (cd -P $(dirname $0) && pwd) | sed -e 's!/bin!!' )
+
+while [ 1 ]; do
+
+	# ora corrente
+	now=$(date +%H:%M)
+
+	# cerco hosts per il backup
+	#hosts=$(cat $INSTALLDIR/etc/scheduler.conf | grep -v '#' | grep $now | awk {'print $1'} | tr \\n ' ')
+	hosts=$(cat $INSTALLDIR/etc/scheduler.conf | grep -v '#' | grep '18:00' | awk {'print $1'} | tr \\n ' ')
+
+	lock="$INSTALLDIR/tmp/$(echo "$hosts" | md5sum | grep -o "[a-z0-9]\{32\}")"
+
+	if [ "$hosts" == "" ]; then
+		echo "No suitable host found ..."
+
+	elif [ ! -e $lock ]; then
+
+		echo "Initializing backups ..."
+	
+		# creo file per il comando da lanciare
+		# evito che venga lanciato all'iterazione successiva
+		touch $lock
+		
+		for i in $hosts; do
+			# ulteriore cofigurazione del comando da laciare ...
+			#pool=$(cat $INSTALLDIR/etc/scheduler.conf | grep -v '#' | grep $now |grep $i| awk {'print $3'})
+			pool=$(cat $INSTALLDIR/etc/scheduler.conf | grep -v '#' | grep '18:00'|grep $i| awk {'print $3'})
+
+			if [ "$pool" == "1" ]; then
+				max_backups=4
+			else
+				max_backups=2
+			fi
+		done 
+		# lancio job-runner.sh && rm $lock
+		echo "Time: $now"
+		echo "Max Backups: $max_backups"
+		echo "Starting: $hosts"
+		
+		(sleep 15 && rm $lock) &
+		#($INSTALLDIR/bin/job-runner.sh -j $max_backups $hosts && rm $lock) &
+	else 
+		echo "Backup still running ..."
+		echo ""
+	fi
+
+	sleep 10
+
+done
