488fa88727026c0af1fd95e6842eda6556f2d052
Enrico Cavalli <cavalli@cilea.it>
2011-03-19 08:12:10 +0100

    uniformata directory di lock logging centralizzato anche di scheduler

--- bin/scheduler.sh
+++ bin/scheduler.sh
@@ -3,6 +3,12 @@
 # demone scheduler
 INSTALLDIR=$( (cd -P $(dirname $0) && pwd) | sed -e 's!/bin!!' )
 
+. $INSTALLDIR/bin/functions
+
+pid=$$
+scriptname=$(basename ${0})
+logfile="$INSTALLDIR/logs/${scriptname}.log"
+
 while [ 1 ]; do
 
 	# ora corrente
@@ -11,18 +17,14 @@ while [ 1 ]; do
 	# TODO: trap segnali per rilettura file di conf 
 	# cerco hosts per il backup
 	hosts=$(cat $INSTALLDIR/etc/scheduler.conf |  sed -e 's/#.*//'| grep -v "^\s*$" | grep $now | awk {'print $1'} | tr \\n ' ')
-	#hosts=$(cat $INSTALLDIR/etc/scheduler.conf | sed -e 's/#.*//'| grep -v "^\s*$" | grep '18:00' | awk {'print $1'} | tr \\n ' ')
 
-	lock="$INSTALLDIR/tmp/$(echo "$hosts" | md5sum | grep -o "[a-z0-9]\{32\}")"
+	lock="$INSTALLDIR/locks/$(echo "$hosts" | md5sum | grep -o "[a-z0-9]\{32\}")"
 
-	if [ "$hosts" == "" ]; then
-		echo "No suitable host found ..." > /dev/null
-	else 
+	if ! [ "$hosts" == "" ]; then
+
 		lockfile -l 3600 -r 0 "$lock" 2>&1
 		return=$?
 		if [ "$return" == "0" ]; then
-
-		#echo "Initializing backups ..."
 	
 		# creo file per il comando da lanciare
 		# evito che venga lanciato all'iterazione successiva
@@ -30,7 +32,6 @@ while [ 1 ]; do
 		for i in $hosts; do
 			# ulteriore cofigurazione del comando da laciare ...
 			pool=$(cat $INSTALLDIR/etc/scheduler.conf |  sed -e 's/#.*//'| grep -v "^\s*$" | grep $now |grep $i| awk {'print $3'})
-			#pool=$(cat $INSTALLDIR/etc/scheduler.conf |  sed -e 's/#.*//'| grep -v "^\s*$" | grep '18:00'|grep $i| awk {'print $3'})
 
 			if [ "$pool" == "1" ]; then
 				max_backups=4
@@ -39,16 +40,12 @@ while [ 1 ]; do
 			fi
 		done 
 		# lancio job-runner.sh && rm $lock
-		echo "Time: $now" >> /tmp/scheduler.log
-		echo "Max Backups: $max_backups" >> /tmp/scheduler.log
-		echo "Starting: $hosts" >> /tmp/scheduler.log
-		echo "--------------------" >> /tmp/scheduler.log
+		
+		log_message ${scriptname} ${pid} "starting ${hosts} (${max_backups})" >> ${logfile}
 		
 		#(sleep 15 && rm $lock) &
 		# TODO: cosa succede se job-runner ritorna non zero? mi resta appeso il file di lock?
-		$INSTALLDIR/bin/job-runner -j $max_backups $hosts  &
-		else 
-		echo "Backup still running ..." > /dev/null
+		$INSTALLDIR/bin/job-runner -j ${max_backups} ${hosts} 2>&1 | log_output ${scriptname} ${pid} >> ${logfile} &
 		fi
 	fi
 
